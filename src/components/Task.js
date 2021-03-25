374876d489ec80651b4e462990e45e9c61e77217
CodingMirec <m.sykora01@gmail.com>
2021-03-25 23:21:42 +0100

    preparations for db

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,7 @@ import "./Task.css";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-function Task() {
+function Task(props) {
   const handleRestart = (e) => {
     e.preventDefault();
 
@@ -22,11 +22,15 @@ function Task() {
     <div className="day__task">
       <div className="day__taskName">
         <span className="taskName">Emails</span>
+        {/* <span className="taskName">{props.name}</span> */}
         <span className="taskProject">Amazon</span>
+        {/* <span className="taskProject">{props.project}</span> */}
       </div>
       <div className="day__taskInfo">
         <span className="taskInfo__time">15:00 - 19:00</span>
+        {/* <span className="taskInfo__time">{props.timeZone}</span> */}
         <span className="timeTotal">4:00</span>
+        {/* <span className="timeTotal">{props.time}</span> */}
         <PlayArrowIcon onClick={handleRestart} />
         <MoreVertIcon onClick={handleDelete} />
       </div>
--- src/components/TrackerDay.js
+++ src/components/TrackerDay.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 
 import "./TrackerDay.css";
 
 function TrackerDay() {
+  const [myTasks, setMyTasks] = useState([]);
+
+  //     TAKETO TU TREBA MAT !!!!
+
+  // const getMyStocks = () => {
+  //   db.collection("myStocks").onSnapshot((snapshot) => {
+  //     let promises = [];
+  //     let tempData = [];
+  //     snapshot.docs.map((doc) => {
+  //       promises.push(
+  //         getStocksData(doc.data().ticker).then((res) => {
+  //           tempData.push({
+  //             id: doc.id,
+  //             data: doc.data(),
+  //             info: res.data,
+  //           });
+  //         })
+  //       );
+  //     });
+  //     Promise.all(promises).then(() => {
+  //       setMyStocks(tempData);
+  //     });
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   let tempStocksData = [];
+  //   const stocksList = [
+  //     "AAPL",
+  //     "MSFT",
+  //     "TSLA",
+  //     "FB",
+  //     "BABA",
+  //     "UBER",
+  //     "DIS",
+  //     "SBUX",
+  //   ];
+
+  //   let promises = [];
+  //   getMyStocks();
+  //   stocksList.map((stock) => {
+  //     promises.push(
+  //       getStocksData(stock).then((res) => {
+  //         console.log(res);
+  //         tempStocksData.push({
+  //           name: stock,
+  //           ...res.data,
+  //         });
+  //       })
+  //     );
+  //   });
+
+  //   Promise.all(promises).then(() => {
+  //     console.log(tempStocksData);
+  //     setStockData(tempStocksData);
+  //   });
+  // }, []);
+
   return (
     <div>
       <div className="tracker__day">
