b81c0e2870fc6c3d549c6de2ca61e0eb66e45710
CodingMirec <m.sykora01@gmail.com>
2021-03-27 19:25:43 +0100

    header chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,28 @@
 import React from "react";
-
 import "./Chat.css";
 
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+
 function Chat() {
-  return <div className="chat">me ame Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <div className="header__left">
+          <h4>
+            # Your Room
+            <StarBorderOutlinedIcon />
+          </h4>
+        </div>
+        <div className="header__right">
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
+      </div>
+      <div className="chat__messages">Messages</div>
+    </div>
+  );
 }
 
 export default Chat;
