f4b134aa076f4ab0dc6fbd48d6e445b9477dac33
CodingMirec <m.sykora01@gmail.com>
2021-04-02 18:33:59 +0200

    dummy message

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,13 +5,22 @@ import "./Message.css";
 function Message() {
   return (
     <div className="message">
-      <img src="" alt="avatar" />
+      <img
+        src="https://cdn.imgbin.com/14/8/7/imgbin-avatar-user-computer-icons-software-developer-avatar-cHpTC4i4gTx3YyY9YUPpaPJGi.jpg"
+        alt="avatar"
+      />
       <div className="message__info">
         <h4>
-          username
+          Your Boss
           <span>Sat, 06 Mar 2021 23:00:47 GMT</span>
         </h4>
-        <p>message stuff here</p>
+        <p>
+          listen here you little faggots, work harder
+          blyataddddddddddddddddddddddd ddddddddddddddd ddddddddddddddd
+          ddddddddddddddddddd ddddddddddddddd ddddddddddddddddddddddddddddd
+          ddddddddddddddddddddddddddddddddddd ddddddddddddddddddddddddddddd
+          ddddddddddddddddddddddddd
+        </p>
       </div>
     </div>
   );
